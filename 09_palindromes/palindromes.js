9733c05a81937d2d5f8ab4c8206b49cf1ffe6c67
Jonathan Mena <jonathanmena123@yahoo.com>
2022-06-15 23:21:27 -0500

    Finished Fundamentals part 5

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newStr = '';
+    for(let i = 0; i < str.length; i++){
+        if(isAlpha(str[i]))
+            newStr += str[i].toLowerCase();
+    }
+    let left = 0;
+    let right = newStr.length - 1;
+    while(left < right){
+        if(newStr[left] === newStr[right]){
+            left++;
+            right--;
+            continue;
+        }
+        return false;
+    }
+    return true;
 };
+
+const isAlpha = function(ch) {
+    return (ch >= "A" && ch <= "Z") || (ch >= "a" && ch <= "z");
+}
 
 // Do not edit below this line
 module.exports = palindromes;
--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    const oldestAge = arr.sort((prev, curr) => {
+        if(!('yearOfDeath' in curr))
+            curr.yearOfDeath = (new Date()).getFullYear();
+        if(!('yearOfDeath' in prev))
+            prev.yearOfDeath = (new Date()).getFullYear();
+        return (prev.yearOfDeath - prev.yearOfBirth) > (curr.yearOfDeath - curr.yearOfBirth) ? 1:-1;
+    });
+    return oldestAge[oldestAge.length - 1]; 
 };
 
 // Do not edit below this line
